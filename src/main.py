0a016e7df047562e80a1216c2235ba3e9db768d8
Steven Baldasty <sbaldasty@gmail.com>
2024-10-24 16:09:54 -0400

    remove useless stuff

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-import threading
-import flwr as fl
-
 from client import client_fn
 
 from dataclasses import dataclass
-from typing import List, Tuple
 
 from flwr.common import Context, Metrics, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig, Server
@@ -18,38 +14,13 @@ from src.attack import SignFlipAttack
 from src.task import Net, get_weights
 from src.strategy import AdversarialScenarioStrategyDecorator, ClientAwaitStrategyDecorator, RaiseOnFailureStrategyDecorator
 
-@dataclass
-class Experiment:
-    pass
+from flwr.simulation import start_simulation
+
 
 def default_fit_config():
     return {
         'batch-size': 32
-#num-server-rounds = 3
-#fraction-evaluate = 0.5
-#local-epochs = 1
-#learning-rate = 0.1
-#batch-size = 32
     }
-
-# Start the Flower server in a separate thread
-def start_server():
-    strategy = fl.server.strategy.FedAvg(fit_config=default_fit_config)
-    fl.server.start_server(
-        server=Server(SimpleClientManager() ),
-        server_address="localhost:8080",
-        config=default_fit_config(),
-        strategy=strategy
-    )
-
-# Start the client simulation in another thread
-def start_simulation():
-    fl.simulation.start_simulation(
-        client_fn=client_fn,
-        num_clients=10,  # Simulate 10 clients
-        config=ServerConfig(num_rounds=3),
-
-    )
 
 if __name__ == "__main__":
     ndarrays = get_weights(Net())
@@ -59,7 +30,7 @@ if __name__ == "__main__":
     strategy = FedAvg(
         fraction_fit=1.0,
         fraction_evaluate=0.5,
-        min_available_clients=2,
+        min_available_clients=10,
         evaluate_metrics_aggregation_fn=src.server.weighted_average,
         initial_parameters=parameters,
     )
@@ -67,47 +38,4 @@ if __name__ == "__main__":
     strategy = RaiseOnFailureStrategyDecorator(strategy)
     strategy = ClientAwaitStrategyDecorator(strategy, 10)
 
-    server = Server(SimpleClientManager(), strategy)
-    # Start server and simulation concurrently
-    server_thread = threading.Thread(target=lambda: fl.server.start_server(
-        server=server,
-        server_address="localhost:8080",
-        config=default_fit_config(),
-        strategy=strategy))
-
-    client_thread = threading.Thread(target=lambda: fl.simulation.start_simulation(
-        client_fn=client_fn,
-        num_clients=10,  # Simulate 10 clients
-        config=ServerConfig(num_rounds=3)))
-
-    # Start both threads
-    server_thread.start()
-    client_thread.start()
-
-    # Wait for both to finish
-    server_thread.join()
-    client_thread.join()
-    
-
-# if __name__ == '__main__':
-#     # Read from config
-#     num_rounds = context.run_config["num-server-rounds"]
-
-#     # Initialize model parameters
-#     ndarrays = get_weights(Net())
-#     parameters = ndarrays_to_parameters(ndarrays)
-
-#     attack = SignFlipAttack()
-#     strategy = FedAvg(
-#         fraction_fit=1.0,
-#         fraction_evaluate=context.run_config["fraction-evaluate"],
-#         min_available_clients=2,
-#         evaluate_metrics_aggregation_fn=weighted_average,
-#         initial_parameters=parameters,
-#     )
-#     strategy = AdversarialScenarioStrategyDecorator(strategy, attack, 2)
-#     strategy = RaiseOnFailureStrategyDecorator(strategy)
-#     strategy = ClientAwaitStrategyDecorator(strategy, 10)
-#     config = ServerConfig(num_rounds=num_rounds)
-
-#     return ServerAppComponents(strategy=strategy, config=config)
+    start_simulation(client_fn=client_fn, num_clients=10, server=Server(SimpleClientManager(), strategy), num_rounds=3, config=ServerConfig(num_rounds=3))
