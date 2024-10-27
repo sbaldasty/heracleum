a0bff1d233c7bce2d3a02d15771a8b98c5914f40
Steven Baldasty <sbaldasty@gmail.com>
2024-10-26 23:11:27 -0400

    poison effect csv

--- src/_poisoneffect.py
+++ src/_poisoneffect.py
@@ -1,3 +1,5 @@
+from dataclass_csv import DataclassWriter
+from dataclasses import dataclass
 from flwr.server import ServerConfig
 from flwr.simulation import start_simulation
 from flwr_datasets import FederatedDataset
@@ -9,17 +11,25 @@ from task import test
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, Normalize, ToTensor
 
+
+OUTPUT_FILE = './out/poisoneffect.csv'
 N_CLIENTS = 10
-N_ROUNDS = 2
+N_ROUNDS = 20
+N_CORRUPT_CLIENTS_START = 0
+N_CORRUPT_CLIENTS_END = 10
+N_CORRUPT_CLIENTS_STEP = 1
+
+
+@dataclass
+class Experiment:
+    n_clients: int
+    n_rounds: int
+    n_corrupt_clients: int
+    loss: float
+    accuracy: float
+
 
 if __name__ == '__main__':
-    server, model = make_cifar_server(n_clients=N_CLIENTS)
-    start_simulation(
-        client_fn=client_fn,
-        num_clients=N_CLIENTS,
-        server=server,
-        config=ServerConfig(num_rounds=N_ROUNDS))
-
     # TODO Somehow reuse this for all the clients
     partitioner = IidPartitioner(num_partitions=N_CLIENTS)
     fds = FederatedDataset(
@@ -35,7 +45,26 @@ if __name__ == '__main__':
         batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
         return batch
 
-    dataset = fds.load_split("test").with_transform(apply_transforms)
-    print(f'THE DATASET IS {dataset}')
-    loss, accuracy = test(model.to(get_device()), DataLoader(dataset))
-    print(f'THE ACCURACY IS {accuracy}')
+    test_dataset = fds.load_split("test").with_transform(apply_transforms)
+    test_loader = DataLoader(test_dataset)
+
+    experiments = []
+
+    for n_corrupt_clients in range(N_CORRUPT_CLIENTS_START, N_CORRUPT_CLIENTS_END + 1, N_CORRUPT_CLIENTS_STEP):
+        server, model = make_cifar_server(n_clients=N_CLIENTS)
+        start_simulation(
+            client_fn=client_fn,
+            num_clients=N_CLIENTS,
+            server=server,
+            config=ServerConfig(num_rounds=N_ROUNDS))
+
+        loss, accuracy = test(model.to(get_device()), test_loader)
+        experiments.append(Experiment(
+            n_clients=N_CLIENTS,
+            n_rounds=N_ROUNDS,
+            n_corrupt_clients=n_corrupt_clients,
+            loss=loss,
+            accuracy=accuracy))
+
+    with open(OUTPUT_FILE, 'w') as file:
+        DataclassWriter(file, experiments, Experiment).write()
